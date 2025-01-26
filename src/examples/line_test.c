94f438a6016f37e3b41142395c1534e129a113a4
Ross Hvidsten <rhvidste@student.hive.fi>
2025-01-26 17:02:08 +0200

    added line test example for how to drawp a line between two pixles with mlx

---
+++ src/examples/line_test.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   line_test.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rhvidste <rvidste@student.42.fr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/26 16:55:14 by rhvidste          #+#    #+#             */
+/*   Updated: 2025/01/26 17:01:44 by rhvidste         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	draw_line(mlx_image_t *img, int x0, int y0, int x1, int y1, uint32_t color)
+{
+	int	dx;
+	int	sx;
+	int	dy;
+	int	sy;
+	int	err;
+	int	e2;
+
+	dx = abs(x1 - x0);
+	sx = x0 < x1 ? 1 : -1;
+	dy = -abs(y1 - y0);
+	sy = y0 < y1 ? 1 : -1;
+	err = dx + dy;
+
+	while (1)
+	{
+		mlx_put_pixel(img, x0, y0, color);
+		if (x0 == x1 && y0 == y1)
+			break;
+		e2 = 2 * err;
+		if (e2 >= dy)
+		{
+			err += dy;
+			x0 += sx;
+		}
+		if (e2 <= dx)
+		{
+			err += dx;
+			y0 += sy;
+		}
+	}
+}
