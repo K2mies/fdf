fd6856d5e61db9210a9781c174e1d2bcae3174ad
Ross Hvidsten <rhvidste@student.hive.fi>
2025-02-12 13:23:56 +0200

    changed rotation angle to reflect true isometric view

--- src/transform_operations.c
+++ src/transform_operations.c
@@ -6,7 +6,7 @@
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:47:42 by rhvidste          #+#    #+#             */
-/*   Updated: 2025/02/04 16:13:16 by rhvidste         ###   ########.fr       */
+/*   Updated: 2025/02/12 13:23:30 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	orthographic_rotation(t_data *data)
 	t_matrix	rot_x;
 	t_matrix	rot_z;
 
-	rot_x = create_rotation_x_matrix(deg_to_rad(45));
+	rot_x = create_rotation_x_matrix(deg_to_rad(25.264));
 	rot_z = create_rotation_z_matrix(deg_to_rad(45));
 	multiply_points(data, &rot_z);
 	multiply_points(data, &rot_x);
