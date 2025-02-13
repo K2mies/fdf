6a2bfb733722d7b61420b2d0d2b1788d8c58edda
Ross Hvidsten <rhvidste@student.hive.fi>
2025-02-13 12:59:43 +0200

    fixed memory leak from mlx

--- src_bonus/bonus_fdf.c
+++ src_bonus/bonus_fdf.c
@@ -6,7 +6,7 @@
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 10:47:40 by rhvidste          #+#    #+#             */
-/*   Updated: 2025/02/12 17:08:26 by rhvidste         ###   ########.fr       */
+/*   Updated: 2025/02/13 12:59:01 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "fdf.h"
@@ -35,7 +35,7 @@ int32_t	main(int argc, char **argv)
 	mlx_scroll_hook(data->mlx, &z_keyhook, data);
 	mlx_loop(data->mlx);
 	mlx_delete_image(data->mlx, data->img);
-	free_all(data, 'n');
 	mlx_terminate(data->mlx);
+	free_all(data, 'n');
 	return (EXIT_SUCCESS);
 }
